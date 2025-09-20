d5d0cc8483ad91cf81fa1b1146ec5a512511fc84
ırmak <nazliirmak2002@gamil.com>
2025-09-20 13:31:58 +0300

    second

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Services from './pages/Services';
+import News from './pages/News';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-white">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
